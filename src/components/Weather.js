2f7e238efc4d5a0dfac6e053ca41603b33cd1116
Toni Naumanen <toni.naumis@gmail.com>
2020-06-11 11:32:51 +0300

    simple loading text when retrieving weather data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,20 +4,27 @@ import axios from 'axios'
 const Weather = ({ match }) => {
   const city = match.params.city
   const [weather, setWeather] = useState(null)
+  const [loading, setLoading] = useState(true)
   // eslint-disable-next-line no-undef
   const api_key = process.env.REACT_APP_API_KEY
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
 
   useEffect(() => {
+    setLoading(true)
     axios
       .get(url)
       .then(response => {
+        setLoading(false)
         console.log('promise fulfilled')
         setWeather(response.data)
       })
-      .catch(setWeather(null))
+      .catch(() => {
+        setLoading(false)
+        setWeather(null)
+      })
   }, [url])
 
+  if (loading) return 'loading...'
   if (weather) {
     return (
       <div>
