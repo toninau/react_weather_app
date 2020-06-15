071e028412d5c90836d0d6a9622c2066d89f8d88
Toni Naumanen <toni.naumis@gmail.com>
2020-06-15 14:53:10 +0300

    simple daily weather graph

--- src/components/Weather/WeatherDetails.js
+++ src/components/Weather/WeatherDetails.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Chart from 'chart.js'
+
+const WeatherGraph = ({ weatherData }) => {
+  const chartRef = React.createRef()
+  const data = weatherData.map(w => w.temp.day)
+  const labels = weatherData.map(w => {
+    const dateObject = new Date(w.dt * 1000)
+    return dateObject.toLocaleString('en-US', { weekday: 'long', day: 'numeric', month: 'numeric' })
+  })
+
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext('2d')
+    new Chart(myChartRef, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Temperatures',
+            data: data,
+          }
+        ]
+      },
+      options: {
+      }
+    })
+  })
+
+  return (
+    <div>
+      <canvas
+        ref={chartRef}
+      />
+    </div>
+  )
+}
 
 const WeatherDetails = ({ weather }) => {
   return (
     <div>
       <h2>details</h2>
-      <div>
-        {weather.daily.map(w => (
-          <div key={w.dt}>
-            <ul>
-              <li>{w.temp.day}</li>
-            </ul>
-          </div>
-        ))}
-      </div>
+      <WeatherGraph weatherData={weather.daily} />
     </div>
   )
 }
