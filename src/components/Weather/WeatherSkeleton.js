d5694839d7737681bcff85c7dbdfc8d8ea80a55b
Toni Naumanen <toni.naumis@gmail.com>
2020-07-08 17:38:16 +0300

    made skeleton loader more detailed

--- src/components/Weather/WeatherSkeleton.js
+++ src/components/Weather/WeatherSkeleton.js
@@ -10,10 +10,37 @@ const WeatherSkeleton = () => {
         <a href="#chart">chart</a>
       </div>
       <div className="weather-container-skeleton">
-        <div className="skeleton-card"></div>
-        <div className="skeleton-card"></div>
-        <div className="skeleton-card"></div>
-        <div className="skeleton-card"></div>
+        <div className="skeleton-card">
+          <div>
+            <div className="skeleton-shape" style={{ height: '32px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '128px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '90px' }}></div>
+          </div>
+          <div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '50px' }}></div>
+            <div className="skeleton-shape" style={{ height: '200px', width: '200px' }}></div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '90px' }}></div>
+          </div>
+        </div>
+        <div className="skeleton-card">
+          <div className="skeleton-shape" style={{ height: '240px', width: '183px' }}></div>
+          <div className="skeleton-shape" style={{ height: '240px', width: '183px' }}></div>
+          <div className="skeleton-shape" style={{ height: '240px', width: '183px' }}></div>
+          <div className="skeleton-shape" style={{ height: '240px', width: '183px' }}></div>
+        </div>
+        <div className="skeleton-card">
+          <div>
+            <div className="skeleton-shape" style={{ height: '32px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '128px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '257px' }}></div>
+            <div className="skeleton-shape" style={{ height: '21px', width: '90px' }}></div>
+          </div>
+          <div className="skeleton-shape" style={{ height: '240px', width: '183px' }}></div>
+        </div>
+        <div className="skeleton-card">
+          <div className="skeleton-shape" style={{ height: '400px', width: '100%' }}></div>
+        </div>
       </div>
     </div>
   )
