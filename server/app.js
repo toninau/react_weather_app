ad6c10d983f8f428452e8e2f1701b48a9bb97de6
Toni Naumanen <toni.naumis@gmail.com>
2020-07-15 16:58:52 +0300

    added rate-limiter for api

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 const weatherRouter = require('./controllers/weather')
 const forecastRouter = require('./controllers/forecast')
 const middleware = require('./utils/middleware')
 
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000, //1 minute
+  max: 6
+})
+
+app.set('trust proxy', 1)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use('/api/', apiLimiter)
 
 app.use('/api/weather', weatherRouter)
 app.use('/api/forecast', forecastRouter)
