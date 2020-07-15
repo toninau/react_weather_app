b00b5e8005302ff23572183cfce9664eec1d716e
Toni Naumanen <toni.naumis@gmail.com>
2020-07-15 16:42:08 +0300

    caching weather and forecast data

--- server/controllers/forecast.js
+++ server/controllers/forecast.js
@@ -4,17 +4,38 @@ const config = require('../utils/config')
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?'
 
+let cachedForecasts = []
+
 forecastRouter.get('/:city', async (request, response, next) => {
+  const city = request.params.city.toLowerCase()
+  const cachedWeather = cachedForecasts.find(c => c.cachedData.city.name.toLowerCase() === city)
+
+  //respond with in memory cache if data less than 15min old
+  if (cachedWeather) {
+    const timeCheck = Date.now() - 900 * 1000
+    if (cachedWeather.cacheTime > timeCheck) {
+      return response.json(cachedWeather.cachedData)
+    } else {
+      //remove all old data
+      cachedForecasts = cachedForecasts.filter(c => c.cacheTime > timeCheck)
+    }
+  }
+
   try {
     const params = new URLSearchParams({
-      q: request.params.city,
+      q: city,
       appid: config.API_KEY,
       units: 'metric'
     })
     const { data } = await axios.get(`${BASE_URL}${params}`)
-    response.json(data)
+    //maximum limit of 3 exceeded, remove oldest
+    if (cachedForecasts.length > 2) {
+      cachedForecasts.shift()
+    }
+    cachedForecasts.push({ cachedData: data, cacheTime: Date.now() })
+    return response.json(data)
   } catch (error) {
-    next(error)
+    return next(error)
   }
 })
 
--- server/controllers/weather.js
+++ server/controllers/weather.js
@@ -4,17 +4,38 @@ const config = require('../utils/config')
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
+let cachedWeathers = []
+
 weatherRouter.get('/:city', async (request, response, next) => {
+  const city = request.params.city.toLowerCase()
+  const cachedWeather = cachedWeathers.find(c => c.cachedData.name.toLowerCase() === city)
+
+  //respond with in memory cache if data less than 5min old
+  if (cachedWeather) {
+    const timeCheck = Date.now() - 300 * 1000
+    if (cachedWeather.cacheTime > timeCheck) {
+      return response.json(cachedWeather.cachedData)
+    } else {
+      //remove all old data
+      cachedWeathers = cachedWeathers.filter(c => c.cacheTime > timeCheck)
+    }
+  }
+
   try {
     const params = new URLSearchParams({
-      q: request.params.city,
+      q: city,
       appid: config.API_KEY,
       units: 'metric'
     })
     const { data } = await axios.get(`${BASE_URL}${params}`)
-    response.json(data)
+    //maximum limit of 5 exceeded, remove oldest
+    if (cachedWeathers.length > 4) {
+      cachedWeathers.shift()
+    }
+    cachedWeathers.push({ cachedData: data, cacheTime: Date.now() })
+    return response.json(data)
   } catch (error) {
-    next(error)
+    return next(error)
   }
 })
 
